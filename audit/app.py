3d17b3032d88c4c7c625e7c0a1dc609a45a545ab
rj <rjgayem@gmail.com>
2022-11-08 19:05:33 -0800

    change the port

--- audit/app.py
+++ audit/app.py
@@ -81,4 +81,4 @@ def get_search_item(index):
 app = connexion.FlaskApp(__name__, specification_dir="")
 app.add_api("openapi.yaml", strict_validation=True, validate_responses=True)
 if __name__ == "__main__":
-    app.run(port=9000)
+    app.run(port=8110)
